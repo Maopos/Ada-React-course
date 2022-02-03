4ebe70f3efc68220a4e026831ee578a590c6dfc4
maopos <mauricioposadamurillo@gmail.com>
2022-02-03 12:12:33 -0500

    swr fetcher

--- src/App.js
+++ src/App.js
@@ -1,11 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import useSWR from "swr";
+
+const fetcher = (...args) => fetch(...args).then((res) => res.json());
 
 function App() {
+  const { data, error } = useSWR(
+    "https://jsonplaceholder.typicode.com/users",
+    fetcher
+  );
+
+  /* fetch("https://jsonplaceholder.typicode.com/todos/1")
+    .then((response) => response.json())
+    .then((json) => console.log(json)); */
+
+  if (error) return <div>failed to load</div>;
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        {" "}
+        {data
+          ? data.map((i) => {
+              return (
+                <div key={i.id}>
+                  <p>{i.name}</p>
+                  <p>{i.email}</p>
+                  <p>{i.username}</p>
+                </div>
+              );
+            })
+          : "loading..."}
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
