341a4cb740b284f95f2f217d3f334626de9b4adc
maopos <mauricioposadamurillo@gmail.com>
2022-02-04 22:03:06 -0500

    Card

---
+++ src/Dogs.jsx
@@ -0,0 +1,49 @@
+import useSWR from "swr";
+
+const fetcher = (...args) => fetch(...args).then((res) => res.json());
+const Dogs = () => {
+  const url = "https://api.thedogapi.com/v1/images/search";
+
+  const { data, error } = useSWR(url, fetcher);
+
+ 
+
+  console.log(data);
+
+  return (
+    <div>
+      {data ? (
+        <center>
+          <h1>Dog App</h1>
+          <div className="card">
+            <img src={data[0].url} alt="img" />
+            {data[0].breeds.length > 0 ? (
+              <div>
+                <p>
+                  <b>Name: </b>
+                  {data[0].breeds[0].name}
+                </p>
+                <p>
+                  <b>Life span: </b>
+                  {data[0].breeds[0].life_span}
+                </p>
+                <p>
+                  <b>Utility: </b>
+                  {data[0].breeds[0].bred_for}
+                </p>
+                <p>
+                  <b>Temperament: </b>
+                  {data[0].breeds[0].temperament}
+                </p>
+              </div>
+            ) : null}
+          </div>
+        </center>
+      ) : (
+        <p>{error}</p>
+      )}
+    </div>
+  );
+};
+
+export default Dogs;
